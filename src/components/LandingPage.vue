e9a628088de59131f70d306f383bcf0d0beb5911
Karthick Sakkaravarthi Vellaisamy <karthicksakkaravarthi@gmail.com>
2021-12-27 19:27:20 +0530

    Intitial

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app >
+
+    <NavigationDrawer/>
+    <AppBar/>
+
+    <v-main>
+      <v-container class="py-8 px-6" fluid>
+        <router-view/>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import NavigationDrawer from './skeleton/NavigationDrawer.vue'
+import AppBar from './skeleton/AppBar.vue'
+export default {
+  components:{
+    NavigationDrawer,
+    AppBar
+  },
+  data: () => ({
+    cards: ["Today", "Yesterday"],
+    
+  }),
+};
+</script>
